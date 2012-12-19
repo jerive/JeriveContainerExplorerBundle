19c3b79768607857cbb9d7a2fa93bf68bf1eb325
jerive <jerome.viveret@gmail.com>
2012-12-19 17:42:01 +0100

    go on

--- DependencyInjection/Dumper/JsonDumper.php
+++ DependencyInjection/Dumper/JsonDumper.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\Dumper\Dumper;
 use Symfony\Component\DependencyInjection\Parameter;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Alias;
 
 class JsonDumper extends Dumper
 {
@@ -13,18 +14,19 @@ class JsonDumper extends Dumper
     {
         $this->nodes = $this->findNodes();
 
-        $this->edges = array();
         foreach ($this->container->getDefinitions() as $id => $definition) {
-            $this->edges[$id] = array_merge(
-                $this->findEdges($id, $definition->getArguments(), true, ''),
-                $this->findEdges($id, $definition->getProperties(), false, '')
-            );
-
-            foreach ($definition->getMethodCalls() as $call) {
-                $this->edges[$id] = array_merge(
-                    $this->edges[$id],
-                    $this->findEdges($id, $call[1], false, $call[0].'()')
+            if ($definition->isPublic()) {
+                $this->nodes[$id]['children'] = array_merge(
+                    $this->findEdges($id, $definition->getArguments(), true, ''),
+                    $this->findEdges($id, $definition->getProperties(), false, '')
                 );
+
+                foreach ($definition->getMethodCalls() as $call) {
+                    $this->nodes[$id]['children'] = array_merge(
+                        $this->nodes[$id]['children'],
+                        $this->findEdges($id, $call[1], false, $call[0])
+                    );
+                }
             }
         }
 
@@ -52,15 +54,17 @@ class JsonDumper extends Dumper
             }
 
             if ($argument instanceof Reference) {
-                if (!$this->container->has((string) $argument)) {
-                    $this->nodes[(string) $argument] = array(
-                        'name' => $name,
-                        'required' => $required,
-                        'class' => ''
-                    );
+                $definition = $this->resolveServiceDefinition((string) $argument);
+                if ($definition instanceof Definition) {
+                    if ($definition->isPublic()) {
+                        $edges[] = (string) $argument;
+                    }
                 }
 
-                $edges[] = array('name' => $name, 'required' => $required, 'to' => $argument);
+                if (!$this->container->has((string) $argument)) {
+                    $this->nodes[(string) $argument] = $name;
+                }
+
             } elseif (is_array($argument)) {
                 $edges = array_merge($edges, $this->findEdges($id, $argument, $required, $name));
             }
@@ -78,31 +82,51 @@ class JsonDumper extends Dumper
     {
         $nodes = array();
 
-        $container = clone $this->container;
+        foreach ($this->container->getServiceIds() as $id) {
+            $definition = $this->resolveServiceDefinition($id);
 
-        foreach ($container->getDefinitions() as $id => $definition) {
-            $nodes[$id] = array(
-                'class' => str_replace('\\', '\\\\', $this->container->getParameterBag()->resolveValue($definition->getClass())),
-                'attributes' => array()
-            );
-
-            $container->setDefinition($id, new Definition('stdClass'));
-        }
-
-        foreach ($container->getServiceIds() as $id) {
-            $service = $container->get($id);
-
-            if (in_array($id, array_keys($container->getAliases()))) {
+            if ($definition instanceof Alias) {
                 continue;
             }
 
-            if (!$container->hasDefinition($id)) {
-                $nodes[$id] = array('
-                    class' => str_replace('\\', '\\\\', get_class($service)),
-                );
+            if ($definition instanceof Definition) {
+                if ($definition->isPublic()) {
+                    $nodes[$id] = array('name' => array(
+                        'class'     => $definition->getClass(),
+                        'public'    => $definition->isPublic(),
+                        'abstract'  => $definition->isAbstract(),
+                        'synthetic' => $definition->isSynthetic(),
+                        'tags'      => array_keys($definition->getTags()),
+                    ));
+                }
+            } else {
+                $nodes[$id] = array('name' => array('class' => get_class($definition)));
             }
         }
 
         return $nodes;
+    }
+
+    /**
+     * Given an array of service IDs, this returns the array of corresponding
+     * Definition and Alias objects that those ids represent.
+     *
+     * @param string $serviceId The service id to resolve
+     *
+     * @return \Symfony\Component\DependencyInjection\Definition|\Symfony\Component\DependencyInjection\Alias
+     */
+    protected function resolveServiceDefinition($serviceId)
+    {
+        if ($this->container->hasDefinition($serviceId)) {
+            return $this->container->getDefinition($serviceId);
+        }
+
+        // Some service IDs don't have a Definition, they're simply an Alias
+        if ($this->container->hasAlias($serviceId)) {
+            return $this->container->getAlias($serviceId);
+        }
+
+        // the service has been injected in some special way, just return the service
+        return $this->container->get($serviceId);
     }
 }
